2e2707ca22e3b02c9ddf5b984232bce7797598ac
dalton <daltonrharmsen@gmail.com>
2024-10-14 10:45:18 +0200

    added sources for telegraaf, volkskrant (incomplete) and nrc

--- src/sources/sources.py
+++ src/sources/sources.py
@@ -121,18 +121,133 @@ class TelegraafNewsSource(NewsSource):
     """
     NewsSource object wrapping Telegraaf.nl
     """
-    ...
+    def get_articles(self) -> dict:
+        """
+        Get articles from Telegraaf.nl and optionally format into the proper JSON formatting.
+        """
+        articles = requests.get(self.api_url + "/api/telegraaf").json()
+        if processed:
+            processed_articles = preprocess_articles(articles)
+
+        return articles
+
+    def get_write_articles(self, filepath: str, processed=True):
+        """
+        Get articles from Telegraaf.nl and optionally format into proper JSON formatting to write to a JSON file.
+        """
+        articles = requests.get(self.api_url + "/api/telegraaf").json()
+        
+        if processed:
+            processed_articles = preprocess_articles(articles)
+        
+        with open(filepath, "w") as file:
+            json.dump(articles, file, indent=4)
+
+    def preprocess_articles(self, articles: dict) -> dict:
+        """
+        Preprocess the Telegraaf.nl articles from their RSS feed format to the proper JSON formatting.
+        """
+        preprocessed_articles = []
+        for article in articles["rss"]["channel"]["item"]:
+            preprocessed_article = {
+                "source_url": article["link"],
+                "source_text": "Telegraaf.nl",
+                "published_datetime": article["pubDate"],
+                "article_title": article["title"],
+                "article_text": article["description"]["p"]["#text"]
+            }
+            preprocessed_articles.append(preprocessed_article)
+
+        return preprocessed_articles
+
 
 
 class VolkskrantNewsSource(NewsSource):
     """
     NewsSource object wrapping Volkskrant.nl
     """
-    ...
+    # TODO RSS feed only contains title and link, no description
+    def get_articles(self) -> dict:
+        """
+        Get articles from Volkskrant.nl and optionally format into the proper JSON formatting.
+        """
+        articles = requests.get(self.api_url + "/api/volkskrant").json()
+        if processed:
+            processed_articles = preprocess_articles(articles)
+
+        return articles
+
+    def get_write_articles(self, filepath: str, processed=True):
+        """
+        Get articles from Volkskrant.nl and optionally format into proper JSON formatting to write to a JSON file.
+        """
+        articles = requests.get(self.api_url + "/api/volkskrant").json()
+        
+        if processed:
+            processed_articles = preprocess_articles(articles)
+        
+        with open(filepath, "w") as file:
+            json.dump(articles, file, indent=4)
+
+    def preprocess_articles(self, articles: dict) -> dict:
+        """
+        Preprocess the Volkskrant.nl articles from their RSS feed format to the proper JSON formatting.
+        """
+        preprocessed_articles = []
+        for article in articles["rss"]["channel"]["item"]:
+            preprocessed_article = {
+                "source_url": article["link"],
+                "source_text": "AD.nl",
+                "published_datetime": article["pubDate"],
+                "article_title": article["title"],
+                "article_text": article["description"]
+            }
+            preprocessed_articles.append(preprocessed_article)
+
+        return preprocessed_articles
+
 
 
 class NRCNewsSource(NewsSource):
     """
     NewsSource object wrapping NRC.nl
     """
-    ...
+    def get_articles(self) -> dict:
+        """
+        Get articles from NRC.nl and optionally format into the proper JSON formatting.
+        """
+        articles = requests.get(self.api_url + "/api/nrc").json()
+        if processed:
+            processed_articles = preprocess_articles(articles)
+
+        return articles
+
+    def get_write_articles(self, filepath: str, processed=True):
+        """
+        Get from NRC.nl articles and optionally format into proper JSON formatting to write to a JSON file.
+        """
+        articles = requests.get(self.api_url + "/api/nrc").json()
+        
+        if processed:
+            processed_articles = preprocess_articles(articles)
+        
+        with open(filepath, "w") as file:
+            json.dump(articles, file, indent=4)
+
+    def preprocess_articles(self, articles: dict) -> dict:
+        """
+        Preprocess the NRC.nl articles from their RSS feed format to the proper JSON formatting.
+        """
+        preprocessed_articles = []
+        for article in articles["rss"]["channel"]["item"]:
+            preprocessed_article = {
+                "source_url": article["link"],
+                "source_text": "NRC.nl",
+                "published_datetime": article["pubDate"],
+                "article_title": article["title"],
+                "article_text": article["description"]
+            }
+            preprocessed_articles.append(preprocessed_article)
+
+        return preprocessed_articles
+
