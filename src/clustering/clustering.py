b97f726220f6d395a1062569ce4aceed5dc3882d
dalton <daltonrharmsen@gmail.com>
2024-10-25 15:45:40 +0200

    added stop word integration and seperated preprocess_article into two functions for clustering/rewrite model purposes

--- src/clustering/clustering.py
+++ src/clustering/clustering.py
@@ -4,6 +4,9 @@ from sklearn.decomposition import PCA
 import torch
 from transformers import AutoTokenizer, AutoModel
 import hdbscan
+from sources.sources import Language
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 class BertClustering():
     """
@@ -19,19 +22,48 @@ class BertClustering():
         self.model_name = "bert-base-multilingual-uncased"
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
         self.model = AutoModel.from_pretrained(self.model_name)
-        self.clusterer = hdbscan.HDBSCAN(min_cluster_size=10, metric="euclidean")
+        self.clusterer = hdbscan.HDBSCAN(min_cluster_size=10,
+                                         metric="euclidean")
+        nltk.download("stopwords")
+        nltk.download("punkt")
 
-    def preprocess_articles(self, articles: list):
+    def remove_stop_words(self, article: str, language: Language):
         """
-        Pre-process the articles to use as model input
+        Remove (language-specific) stop words from the article text.
+        """
+        stopwords_set = set(stopwords.words(language))
+        article_wordslist = word_tokenize(article)
+        filtered_words = filter(lambda word:
+                                    word.lower() not in stop_words,
+                                    article_wordslist)
+        article_nostop = ' '.join(filtered_words)
+        return article_nostop
+
+    def preprocess_articles(self, sources_articles: list):
+        """
+        Pre-process the articles to use as model input for embedding clustering.
         """
         processed_articles = []
+        for source, article in sources_articles:
+            article_title = article["article_title"]
+            article_text = article["article_text"]
+            article_title_nostop = self.remove_stop_words(article, source.language)
+            article_text_nostop = self.remove_stop_words(article, source.language)
+            text_article = f"# Title:\n{article_title_nostop}\n\n# Text:\n{article_text}"
+            processed_articles.append(text_article)
+        return processed_articles
+
+    def format_articles(self, articles: list):
+        """
+        Format the articles to use as model input for rewriting.
+        """
+        formatted_articles = []
         for article in articles:
             article_title = article["article_title"]
             article_text = article["article_text"]
-            text_article = f"# Title:\n{article_title}\n\n#Text:\n{article_text}"
-            processed_articles.append(text_article)
-        return processed_articles
+            text_article = f"# Title:\n{article_title}\n\n# Text:\n{article_text}"
+            formatted_articles.append(text_article)
+        return formatted_articles
 
     def get_embeddings(self, articles: list):
         """
